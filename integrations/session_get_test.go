fe34b1a86caff2f21f25c0000a11057dcc890153
Lunny Xiao <xiaolunwen@gmail.com>
2021-06-12 16:28:41 +0800

    Add test for get map with NULL column

--- integrations/session_get_test.go
+++ integrations/session_get_test.go
@@ -347,6 +347,28 @@ func TestGetSlice(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func TestGetMap(t *testing.T) {
+	assert.NoError(t, PrepareEngine())
+
+	type UserinfoMap struct {
+		Uid   int `xorm:"pk autoincr"`
+		IsMan bool
+	}
+
+	assertSync(t, new(UserinfoMap))
+
+	_, err := testEngine.Exec("INSERT INTO userinfo_map (is_man) VALUES (NULL)")
+	assert.NoError(t, err)
+
+	var valuesString = make(map[string]string)
+	has, err := testEngine.Table("userinfo_map").Get(&valuesString)
+	assert.NoError(t, err)
+	assert.Equal(t, true, has)
+	assert.Equal(t, 2, len(valuesString))
+	assert.Equal(t, "1", valuesString["uid"])
+	assert.Equal(t, "", valuesString["is_man"])
+}
+
 func TestGetError(t *testing.T) {
 	assert.NoError(t, PrepareEngine())
 
