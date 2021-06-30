ae0d5506c1345a8782558ded656b5950ef793699
Lunny Xiao <xiaolunwen@gmail.com>
2021-06-30 11:24:47 +0800

    Remove unrelated files

--- scan.go
+++ scan.go
@@ -6,57 +6,9 @@ package xorm
 
 import (
 	"database/sql"
-	"time"
 
-	"xorm.io/xorm/convert"
 	"xorm.io/xorm/core"
 )
-
-func (engine *Engine) scan(rows *core.Rows, types []*sql.ColumnType, v ...interface{}) error {
-	var v2 = make([]interface{}, 0, len(v))
-	var turnBackIdxes = make([]int, 0, 5)
-	for i, vv := range v {
-		switch vv.(type) {
-		case *time.Time:
-			v2 = append(v2, &sql.NullString{})
-			turnBackIdxes = append(turnBackIdxes, i)
-		case *sql.NullTime:
-			v2 = append(v2, &sql.NullString{})
-			turnBackIdxes = append(turnBackIdxes, i)
-		default:
-			v2 = append(v2, v[i])
-		}
-	}
-	if err := rows.Scan(v2...); err != nil {
-		return err
-	}
-	for _, i := range turnBackIdxes {
-		switch t := v[i].(type) {
-		case *time.Time:
-			var s = *(v2[i].(*sql.NullString))
-			if !s.Valid {
-				break
-			}
-			dt, err := convert.String2Time(s.String, engine.DatabaseTZ, engine.TZLocation)
-			if err != nil {
-				return err
-			}
-			*t = *dt
-		case *sql.NullTime:
-			var s = *(v2[i].(*sql.NullString))
-			if !s.Valid {
-				break
-			}
-			dt, err := convert.String2Time(s.String, engine.DatabaseTZ, engine.TZLocation)
-			if err != nil {
-				return err
-			}
-			t.Time = *dt
-			t.Valid = true
-		}
-	}
-	return nil
-}
 
 func (engine *Engine) row2mapStr(rows *core.Rows, types []*sql.ColumnType, fields []string) (map[string]string, error) {
 	var scanResults = make([]interface{}, len(fields))
