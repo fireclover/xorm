ee1372208150aaa0cd36075bd050b690e7462e62
arturwwl <>
2021-11-22 16:39:18 +0100

    convert - String2Time accept HH:mm:ss format

--- convert/time.go
+++ convert/time.go
@@ -15,6 +15,7 @@ import (
 )
 
 // String2Time converts a string to time with original location
+// be aware for time strings (HH:mm:ss) returns zero year (LMT) for converted location
 func String2Time(s string, originalLocation *time.Location, convertedLocation *time.Location) (*time.Time, error) {
 	if len(s) == 19 {
 		if s == utils.ZeroTime0 || s == utils.ZeroTime1 {
@@ -32,6 +33,7 @@ func String2Time(s string, originalLocation *time.Location, convertedLocation *t
 			return nil, err
 		}
 		dt = dt.In(convertedLocation)
+		dt.IsZero()
 		return &dt, nil
 	} else if len(s) == 25 && s[10] == 'T' && s[19] == '+' && s[22] == ':' {
 		dt, err := time.Parse(time.RFC3339, s)
@@ -47,6 +49,18 @@ func String2Time(s string, originalLocation *time.Location, convertedLocation *t
 			return nil, err
 		}
 		dt = dt.In(convertedLocation)
+		return &dt, nil
+	} else if len(s) == 8 && s[2] == ':' && s[5] == ':' {
+		currentDate := time.Now()
+		dt, err := time.ParseInLocation("15:04:05", s, originalLocation)
+		if err != nil {
+			return nil, err
+		}
+		// add current date for correct time locations
+		dt = dt.AddDate(currentDate.Year(), int(currentDate.Month()), currentDate.Day())
+		dt = dt.In(convertedLocation)
+		// back to zero year
+		dt = dt.AddDate(-currentDate.Year(), int(-currentDate.Month()), -currentDate.Day())
 		return &dt, nil
 	} else {
 		i, err := strconv.ParseInt(s, 10, 64)
