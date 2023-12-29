529b8c66e9c1aa45700502d878613f9a9bd34dfc
Lunny Xiao <xiaolunwen@gmail.com>
2023-12-29 16:57:58 +0800

    Fix use hint with join bug

--- tests/session_test.go
+++ tests/session_test.go
@@ -78,4 +78,11 @@ func TestIndexHint(t *testing.T) {
 
 	_, err = testEngine.Table("userinfo").IndexHint("USE", "GROUP BY", "UQE_userinfo_username").Get(new(Userinfo))
 	assert.NoError(t, err)
+
+	// with join
+	_, err = testEngine.Table("userinfo").
+		Join("LEFT", "userdetail", "userinfo.id = userdetail.id").
+		IndexHint("USE", "", "UQE_userinfo_username").
+		Get(new(Userinfo))
+	assert.NoError(t, err)
 }
