648bcb659814c2d0d32372383f93749fdac8dc7f
zzdboy <28206697@qq.com>
2024-02-27 21:57:13 +0800

    Add Version

---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright 2015 The Xorm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xorm
+
+// Version is the current xorm version.
+const Version = "v1.3.8"
